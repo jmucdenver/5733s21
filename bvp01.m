d013653ca7f74b9070b63f54186fb6132a96d246
Jan Mandel <jan.mandel@gmail.com>
2021-02-08 23:33:09 -0700

    update

--- bvp01.m
+++ bvp01.m
@@ -1,7 +1,15 @@
-function [u,x]=bvp(f,n)
+function [u,x]=bvp01(f,n)
 % [u,x]=bvp(f,n)
 % solve -u''=f, u(0)=u(1)=0 on uniform mesh x(0) to x(n+1)=1
-% Jan Mandel, Feb 10 2020 
+% in
+%   f  function handle
+%   n  mesh size, nodes x(0)=0 to x(n+1)=1
+% out
+%   u  vector of solution values at nodes x
+%   x  coordinates of the nodes
+
+% Jan Mandel, Feb 8 2021
+
 h=1/(n+1);
 x=[1:n]'/(n+1); 
 b=h*h*f(x);

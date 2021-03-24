4ea51a43d9d1eb581656fab8f187bd94e4e3658e
Jan Mandel <jan.mandel@gmail.com>
2021-03-24 11:57:21 -0600

    traveling wave

--- plot_wave_test.m
+++ plot_wave_test.m
@@ -5,6 +5,14 @@ f2=@(x)sin(4*x);
 g=@(x)sin(x);
 z=@(x)0*x;
 
+f3=@(x)exp(-0.5*abs(x-0.5).^2);
+
+input('traveling wave left\n press enter when ready') 
+plot_wave( f3,z   ,1,-5,5,0,10)
+input('traveling wave left\n press enter when ready') 
+plot_wave( z,f3   ,1,-5,5,0,10)
+
+
 input('sine wave left\n press enter when ready') 
 plot_wave( f,z   ,1,-5,5,0,10)
 input('sine wave right\n press enter when ready')
@@ -18,3 +26,5 @@ input('sine wave right\n press enter when ready')
 plot_wave( z,g   ,1,-5,5,0,10)
 input('superposition\n press enter when ready')
 plot_wave( f2,g   ,1,-5,5,0,10)
+
+
